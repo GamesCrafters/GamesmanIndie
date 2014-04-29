dbefc9d187ca86e07cb4e35b8419a89d6195ca8a
rserickson92 <rserickson92@gmail.com>
2014-04-28 23:41:41 -0700

    add eval function

--- board.js
+++ board.js
@@ -1,4 +1,3 @@
-//TODO: replace color1, color2 w/ actual colors
 function Board(x, y) {
     this._board = new Array(x);
     this._width = x;
@@ -9,25 +8,27 @@ function Board(x, y) {
 	this._valid_moves.push(i);
     }
     this._connections = {
-	color1: { h: {}, v: {}, d1: {}, d2: {} },
-	color2: { h: {}, v: {}, d1: {}, d2: {} }
+	R: { h: {}, v: {}, d1: {}, d2: {} },
+	B: { h: {}, v: {}, d1: {}, d2: {} }
     };
-    this._win = { color1: false, color2: false };
+    this._win = { R: false, B: false };
     var pos;
     for(var i = 0; i < this._width; i++) {
 	for(var j = 0; j < this._height; j++) {
 	    pos = [i, j];
-	    connections[color1].h[pos] = 0;
-	    connections[color1].v[pos] = 0;
-	    connections[color1].d[pos] = 0;
-	    connections[color2].h[pos] = 0;
-	    connections[color2].v[pos] = 0;
-	    connections[color2].d[pos] = 0;
+	    this._connections.R.h[pos] = 0;
+	    this._connections.R.v[pos] = 0;
+	    this._connections.R.d1[pos] = 0;
+	    this._connections.R.d2[pos] = 0;
+	    this._connections.B.h[pos] = 0;
+	    this._connections.B.v[pos] = 0;
+	    this._connections.B.d1[pos] = 0;
+	    this._connections.R.d2[pos] = 0;
 	}
     }
     this._move_history = [];
     this._getOtherColor = function(color) {
-	return color == color1 ? color2 : color1;
+	return color === "R" ? "B" : "R";
     };
 
     var next_fns = {
@@ -81,24 +82,38 @@ function Board(x, y) {
     this._removeConnections = function(pos, color) {
 	var connections = this._connections[color];
 	var x = pos[0], y = pos[1], i;
-	var value = connections.h[pos];
+	var value_remove = connections.h[pos];
+	var value;
+	var positions_to_set;
 	connections.h[pos] = 0;
 	connections.v[pos] = 0;
 	connections.d1[pos] = 0;
 	connections.d2[pos] = 0;
 	var o, i, cur_pos;
+	this._win[color] = false;
 	for(var d in ["n", "s", "e", "w", "ne", "sw", "nw", "se"]) {
-	    o = orientations[d];
-	    i = 1;
-	    while(this._isValidPos(cur_pos) && 
-		  connections[o][cur_pos] == value) {
+	    value = 0;
+	    for(i = 1; i <= 3; i++) {
 		cur_pos = next_fns[d](i);
-		connections[o][cur_pos];
-		i++;
+		o = orientations[d];
+		if(this._isValidPos(cur_pos) && connections[o][cur_pos] == value_remove) {
+		    value++;
+		    positions_to_set.push(cur_pos);
+		} else {
+		    break;
+		}
 	    }
-	}
+	    if(value >= 4) { this._win[color] = true; }
+	    for(var p in positions_to_set) {
+		connections[o][p] = value;
+	    }
+	} 
     };
-    this._isValidPos = function(pos) {};
+    this._isValidPos = function(pos) {
+	var x = pos[0], y = pos[1];
+	return x >= 0 && x < this._width &&
+	       y >= 0 && y < this._height;
+    };
 }
 
 Board.prototype.currentColor = function() {
@@ -106,7 +121,7 @@ Board.prototype.currentColor = function() {
 }
 
 Board.prototype.makeMove = function(column, color) {
-    var pos = [column, _board[column].length];
+    var pos = [column, this._board[column].length];
     this._board[column].push(column);
     if(this._board[column].length === this._height) {
 	_valid_moves.splice(column, 1);
@@ -115,7 +130,7 @@ Board.prototype.makeMove = function(column, color) {
     this._addConnections(pos, color);
 };
 Board.prototype.undoLastMove = function() {
-    var last_move = _move_history.pop();
+    var last_move = this._move_history.pop();
     var board = this._board[last_move.column];
     var pos = [last_move.column, board.length];
     if(board.length == this._height) {
@@ -126,13 +141,15 @@ Board.prototype.undoLastMove = function() {
     this._removeConnections(pos, last_move.color);
 };
 
-Board.prototype.isWin(color) = function() {
+Board.prototype.evaluate = function(color) {};
+
+Board.prototype.isWin = function(color) {
     return this._win[color];
 };
-Board.prototype.isLoss(color) = function() {
+Board.prototype.isLoss = function(color) {
     return this.isWin(this._getOtherColor(color));
 };
-Board.prototype.isTie(color) = function() {
+Board.prototype.isTie = function(color) {
     return !this.isWin(color) && !this.isLoss(color) &&
            this._valid_moves.length == 0;
 };
@@ -149,9 +166,9 @@ Board.prototype.toString = function() {
     str += " \n";
     for(var j = this._height-1; j >= 0; j--) {
 	for(var i = 0; i < this._width; i++) {
-	    if (board[i][j] === color1) {
+	    if (board[i][j] === "R") {
 		str += "|R";
-	    } else if(board[i][j] === color2) {
+	    } else if(board[i][j] === "B") {
 		str += "|B";
 	    } else {
 		str += "|_";
@@ -163,33 +180,6 @@ Board.prototype.toString = function() {
 
 /***********************************UNIT TESTS*********************************/
 
-/** rvwin 
- *          _ _ _ _ _ _ 
- *         |_|_|_|_|_|_|
- *         |_|_|_|_|_|_|
- *         |_|_|_|R|_|_|
- *         |_|_|_|R|_|_|
- *         |_|_|B|R|B|_|
- *         |_|R|B|R|B|_|
- */
-/** rhwin 
- *          _ _ _ _ _ _
- *         |_|_|_|_|_|_|
- *         |_|_|_|_|_|_|
- *         |_|_|_|_|_|_|
- *         |B|_|_|_|_|_|
- *         |B|_|R|R|R|R|
- *         |B|R|B|R|B|B|
- */
-/** rdwin 
- *          _ _ _ _ _ _
- *         |_|_|_|_|_|_|
- *         |_|_|_|_|_|_|
- *         |_|_|_|_|R|_|
- *         |_|_|_|R|B|_|
- *         |_|_|R|B|B|_|
- *         |_|R|B|B|R|R|
- */
 function runBoardTests() {
     var passed = 0;
     var results = loadAndRunTests();
@@ -220,28 +210,129 @@ function loadAndRunTests() {
 	result.answer = data.funcs[i].apply(null, data.args[i]);
 	results.push(result);
     }
+    return results;
 }
 
 function loadTestData() {
-    return {
+    var board_data = loadBoards();
+    var test_data = {
 	names: [
 	    "rvwin_init",
 	    "rhwin_init",
 	    "rdwin_init",
-	    "rvwin",
-	    "rhwin",
-	    "rdwin"
+	    "rvwin4",
+	    "rhwin4",
+	    "rdwin4"
 	],
 	expected: [
 	    true,
 	    true,
+	    true,
+	    true,
+	    true, 
 	    true
 	],
-	board_strs: [
-	],
 	func: [
+	    board_eq,
+	    board_eq,
+	    board_eq,
+	    board_data[0].board.isWin,
+	    board_data[1].board.isWin,
+	    board_data[2].board.isWin
 	],
 	args: [
-	]
+	    [board_data[0].board.toString(), board_data[0].str],
+	    [board_data[1].board.toString(), board_data[1].str],
+	    [board_data[2].board.toString(), board_data[2].str],
+	    ["R"],
+	    ["R"],
+	    ["R"]
+	],
+	board_strs: []
     };
-};
+    for(var b in board_data) {
+	test_data.board_strs.push(b.toString());
+    }
+}
+
+function loadBoards() {
+    var board_data = [];
+
+    var rvwin4 = new Board(6, 6);
+    rvwin4.makeMove(1, "R");
+    rvwin4.makeMove(2, "B");
+    rvwin4.makeMove(3, "R");
+    rvwin4.makeMove(2, "B");
+    rvwin4.makeMove(3, "R");
+    rvwin4.makeMove(4, "B");
+    rvwin4.makeMove(3, "R");
+    rvwin4.makeMove(4, "B");
+    rvwin4.makeMove(3, "R");
+    board_data.push({
+	board: rvwin4,
+	str: " _ _ _ _ _ _ \n|_|_|_|_|_|_|\n|_|_|_|_|_|_|\n|_|_|_|R|_|_|\n|_|_|_|R|_|_|\n|_|_|B|R|B|_|\n|_|R|B|R|B|_|\n"
+    });
+
+    var rhwin4 = new Board(6, 6);
+    rhwin4.makeMove(1, "R");
+    rhwin4.makeMove(2, "B");
+    rhwin4.makeMove(2, "R");
+    rhwin4.makeMove(4, "B");
+    rhwin4.makeMove(3, "R");
+    rhwin4.makeMove(5, "B");
+    rhwin4.makeMove(3, "R");
+    rhwin4.makeMove(0, "B");
+    rhwin4.makeMove(4, "R");
+    rhwin4.makeMove(0, "B");
+    rhwin4.makeMove(5, "R");
+    board_data.push({
+	board: rhwin4,
+	str: " _ _ _ _ _ _ \n|_|_|_|_|_|_|\n|_|_|_|_|_|_|\n|_|_|_|_|_|_|\n|_|_|_|_|_|_|\n|B|_|R|R|R|R|\n|B|R|B|R|B|B|\n"
+    });
+
+    var rd1win4 = new Board(6, 6);
+    rd1win4.makeMove(1, "R");
+    rd1win4.makeMove(2, "B");
+    rd1win4.makeMove(4, "R");
+    rd1win4.makeMove(3, "B");
+    rd1win4.makeMove(5, "R");
+    rd1win4.makeMove(3, "B");
+    rd1win4.makeMove(2, "R");
+    rd1win4.makeMove(4, "B");
+    rd1win4.makeMove(3, "R");
+    rd1win4.makeMove(4, "B");
+    rd1win4.makeMove(4, "R");
+    board_data.push({
+	board: rd1win4,
+	str: " _ _ _ _ _ _ \n|_|_|_|_|_|_|\n|_|_|_|_|_|_|\n|_|_|_|_|R|_|\n|_|_|_|R|B|_|\n|_|_|R|B|B|_|\n|_|R|B|B|R|R|\n"
+    });
+    return board_data;
+}
+
+/** rvwin4
+ *          _ _ _ _ _ _ 
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|R|_|_|
+ *         |_|_|_|R|_|_|
+ *         |_|_|B|R|B|_|
+ *         |_|R|B|R|B|_|
+ */
+/** rhwin4 
+ *          _ _ _ _ _ _
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|_|_|
+ *         |B|_|R|R|R|R|
+ *         |B|R|B|R|B|B|
+ */
+/** rd1win4 
+ *          _ _ _ _ _ _
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|_|_|
+ *         |_|_|_|_|R|_|
+ *         |_|_|_|R|B|_|
+ *         |_|_|R|B|B|_|
+ *         |_|R|B|B|R|R|
+ */
